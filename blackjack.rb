62ba71af7349a12fba85a12f5c1ffd48a786db98
Travis Riggs <travis.a.riggs@gmail.com>
2014-04-24 20:14:09 -0400

    * Completed the Blackjack controller class. The game now works.

--- blackjack.rb
+++ blackjack.rb
@@ -1,3 +1,4 @@
+#$$$$$$$$$$$$$$ CLASSES $$$$$$$$$$$$$$$
 class Card
 
   attr_reader :value, :suit
@@ -25,6 +26,8 @@ class Shoe
   def initialize(num_of_decks)
     # Input:
     #  Integer number of decks to use in shoe
+    @num_of_decks = num_of_decks
+    @MIN_SHOE_SIZE = 12
     fill_shoe(num_of_decks)
     shuffle_cards!
   end
@@ -63,7 +66,16 @@ class Shoe
   end
 
   def deal_a_card!
+
+    # Make sure we have enough cards
+    if num_of_cards < @MIN_SHOE_SIZE
+      puts 'Shoe was getting low on cards...'
+      fill_shoe(@num_of_decks)
+      shuffle_cards!
+    end
+
     @cards.pop
+
   end
 
 end
@@ -175,15 +187,19 @@ class BlackjackBasePlayer < BasePlayer
     @hand = BlackjackHand.new
   end
 
+  def new_hand
+    @hand = BlackjackHand.new
+  end
+
   def show_hand(mask=false)
 
     total = hand.calculate
 
     puts
     if mask
-      puts "   Dealer's hand is: ??"
+      puts "   #{name}'s hand is: ??"
     else
-      puts "   Dealer's hand is: #{total}"
+      puts "   #{name}'s hand is: #{total}"
     end
 
     hand.each_with_index { | card, index |
@@ -228,6 +244,221 @@ class BlackjackDealer < BlackjackBasePlayer
 end
 
 
+class Blackjack
+
+  def say(message)
+
+    puts
+    puts ' '*10 + message.to_s
+    puts
+
+    sleep(2.0)
+
+  end
+
+  def get_wager(total_money)
+
+    # Make sure we keep asking until we get a valid bet
+    while true
+
+      say 'How much would you like to bet?'
+      bet = gets.chomp
+
+      if bet.to_i == 0
+        say 'You have to bet something'
+      elsif bet.to_i > total_money
+        puts "You don't have that much money."
+        puts "You can bet a max of $#{total_money}."
+      else
+        return bet.to_i
+      end
+
+    end
+
+  end
+
+  def get_y_n_answer(question, correction='You need to answer y or n')
+    # Ask a yes or no question until the user provides a 'y' or 'n'
+    #  Returns a string 'y' or 'n'
+    #  On a failure, it could possibly return nil
+
+    response = nil
+
+    # Keep asking until we get a proper response
+    while true
+
+      say question
+      response = gets.chomp.downcase
+
+      if ['y', 'n'].include?(response)
+        break
+      else
+        say correction
+      end
+
+    end
+
+    response
+
+  end
+
+  def run
+
+    # Create the Dealer
+    dealer = BlackjackDealer.new
+
+    # Create shoe
+    shoe = Shoe.new(2)
+
+    # Ask who's playing
+    say ' '*10 + 'Welcome to Blackjack!'
+    say "What's your name?"
+
+    user_name = gets.chomp
+
+    # Create player object
+    player = BlackjackPlayer.new(user_name)
+
+    # Be friendly...even though you're going to take their money
+    say "Hello #{player.name}!  Nice to meet you!"
+
+    # How much money will they start with?
+    say 'How much money would you like to convert?'
+
+    starting_money = gets.chomp.to_i
+    player.money = starting_money
+
+
+    # Loop through each hand that the user wants to play
+    #  ...as long as they have money, of course...
+    while player.money > 0
+
+      # Ask for the user's bet
+      wager = get_wager(player.money)
+
+      # It's time to deal the initial cards
+      player.add_card(shoe.deal_a_card!)
+      dealer.add_card(shoe.deal_a_card!)
+
+      player.add_card(shoe.deal_a_card!)
+      dealer.add_card(shoe.deal_a_card!)
+
+      # Display cards
+      dealer.show_partial_hand
+      player.show_hand
+
+      # Check totals here to see if there was blackjack
+      user_total = player.hand.calculate
+
+      user_status = player.hand.status
+
+      # Loop through user 'hits'
+      while user_status == 'Playing'
+
+        response = get_y_n_answer("Would you like a hit, #{player.name}? (y/n)")
+
+        if response == 'y'
+
+          player.add_card(shoe.deal_a_card!)
+
+          # Calculate hand again...did we get to 21?
+          user_total = player.hand.calculate
+
+          player.show_hand
+
+          # How did the user do?
+          if player.hand.status == 'Blackjack'
+            say 'Blackjack baby!'
+            break
+          elsif player.hand.status == 'Bust'
+            say "Sorry #{player.name}...you busted"
+            break
+          end
+
+        elsif response == 'n'
+          break
+        else
+          next
+        end
+
+      end
+
+      # Now it's the dealer's turn...(unless the user had Blackjack or bust)
+      dealer.show_hand
+
+      if player.hand.status == 'Playing'
+
+        # Check totals here to see if there was blackjack
+        dealer_total = dealer.hand.calculate
+
+        # Loop through user 'hits'
+        while dealer_total < 17
+
+          say 'Dealer takes a hit...'
+          dealer.add_card(shoe.deal_a_card!)
+
+          # Calculate hand again...did we get to 21?
+          dealer_total = dealer.hand.calculate
+
+          dealer.show_hand
+
+        end
+
+      end
+
+      # update the gains/losses
+      if player.hand.status == 'Blackjack'
+        say "#{player.name}, you won!"
+        player.money += wager * 1.5
+      elsif player.hand.status == 'Bust'
+        say 'You lost this round.'
+        player.money -= wager
+      elsif player.hand.total > dealer.hand.total || dealer.hand.total > 21
+        say "#{player.name}, you won!"
+        player.money += wager
+      elsif player.hand.total < dealer.hand.total
+        say "Sorry #{player.name}, you lost this time."
+        player.money -= wager
+      elsif player.hand.total == dealer.hand.total
+        say "It's a push."
+      else
+        say "SOMETHING WENT TERRIBLY WRONG!"
+      end
+
+      # Show results of betting
+      say "#{player.name}, you have $#{player.money} left."
+
+      # Reset hands
+      player.new_hand
+      dealer.new_hand
+
+      # Make decisions about the next round
+      break if player.money == 0
+
+      answer = get_y_n_answer("Would you like to play another round, #{player.name}? (y/n)")
+
+      break if answer == 'n'
+
+    end  # End the Game Loop
+
+    # Tell the user how they fared
+    say "You started with $#{starting_money} and ended with $#{player.money}."
+
+    if player.money > starting_money
+      say "Congratulations, #{player.name}!"
+    else
+      say 'Sorry about your luck...'
+    end
+
+    # Say goodbye!
+    say "Thanks for playing #{player.name}! Come back soon!"
+
+  end
+
+
+
+end
+
 
 ########## Main Program ##################
 if __FILE__ == $0
@@ -246,27 +477,30 @@ if __FILE__ == $0
   #
   # puts
 
-  d = BlackjackDealer.new
-  d.show_hand
+  # d = BlackjackDealer.new
+  # d.show_hand
+  #
+  # p = BlackjackPlayer.new('Travis')
+  # p.show_hand
+  #
+  # # Create shoe
+  # shoe = Shoe.new(2)
+  #
+  # # Deal cards
+  # d.add_card(shoe.deal_a_card!)
+  # p.add_card(shoe.deal_a_card!)
+  #
+  # d.add_card(shoe.deal_a_card!)
+  # p.add_card(shoe.deal_a_card!)
+  #
+  # d.show_partial_hand
+  # d.show_hand
+  # p.show_hand
+  #
+  # # puts p.hand.calculate
 
-  p = BlackjackPlayer.new('Travis')
-  p.show_hand
-
-  # Create shoe
-  shoe = Shoe.new(2)
-
-  # Deal cards
-  d.add_card(shoe.deal_a_card!)
-  p.add_card(shoe.deal_a_card!)
-
-  d.add_card(shoe.deal_a_card!)
-  p.add_card(shoe.deal_a_card!)
-
-  d.show_partial_hand
-  d.show_hand
-  p.show_hand
-
-  # puts p.hand.calculate
+  game = Blackjack.new
+  game.run
 
 
 end
